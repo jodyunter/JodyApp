2bb87b17593e6061117a0e65bbc4adc0143708d3
jody_unterschutz <jody_unterschutz@transcanada.com>
2018-03-09 10:51:13 -0700

    more season stuff

---
+++ JodyApp/Domain/Season/SeasonGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JodyApp.Domain.Season
+{
+    public class SeasonGame:Game
+    {
+        public Season Season { get; set; }
+    }
+}
