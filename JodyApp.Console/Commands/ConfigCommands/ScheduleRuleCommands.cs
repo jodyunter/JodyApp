5bd19bdbb05114ef743b06b5114885b3d8cb7398
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-06-28 13:19:03 -0600

    m

---
+++ JodyApp.Console/Commands/ConfigCommands/ScheduleRuleCommands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JodyApp.ConsoleApp.Views;
+using JodyApp.ViewModel;
+
+namespace JodyApp.ConsoleApp.Commands
+{
+    public class ScheduleRuleCommands : BaseViewCommands
+    {
+        public override Func<ApplicationContext, string, ReferenceObject> SelectMethod => throw new NotImplementedException();
+
+        public override Action<ApplicationContext> ClearSelectedItem => throw new NotImplementedException();
+
+        public override BaseViewModel ConstructViewModelFromData(Dictionary<string, string> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<string, string> GatherCreateData(ApplicationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BaseListView GetList(ListViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BaseView GetView(BaseViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
