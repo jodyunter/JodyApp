6164dd71ff404341c7e857b5dc198eaa782c4ab4
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-07-03 14:13:30 -0600

    m

--- JodyApp.Console/Views/Competition/Config/Division/ScheduleRuleView.cs
+++ JodyApp.Console/Views/Competition/Config/Division/ScheduleRuleView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JodyApp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace JodyApp.ConsoleApp.Views
 {
-    public class ScheduleRuleView
+    public class ScheduleRuleView:BaseView
     {
+        public override string[] ViewHeaders => new string[] { "Id", "Name", "Short", "League", "Season", "Parent", "Level", "Order" };
+        public override object[] ViewObjects
+        {
+            get
+            {
+                var m = (ScheduleRuleViewModel)Model;
+                return new object[] { m.Id, m.Name, m.ShortName, m.League,
+                    m.Season != null ? m.Season.Name : "None",
+                    m.Parent != null ? m.Parent.Name :"None",
+                    m.Level, m.Order };
+            }
+        }
+
+        public ScheduleRuleView(BaseViewModel model) : base(model) { }
     }
 }
