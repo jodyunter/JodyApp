4ccccbff2b9088be86d51380e28950a36ed053b9
Jody Unterschutz <jody_unterschutz@transcanada.com>
2018-07-05 08:25:22 -0600

    m

--- JodyApp.Console/Views/Competition/Config/Division/ScheduleRuleView.cs
+++ JodyApp.Console/Views/Competition/Config/Division/ScheduleRuleView.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace JodyApp.ConsoleApp.Views
 {
-    public class ScheduleRuleView:BaseView
+    public class ScheduleRuleView : BaseView
     {
         public override string[] ViewHeaders => new string[] { "Id", "Name", "Home Type", "Home Team", "Home Div",
             "Away Type", "Away Team", "Away Div", "Home&Away", "Rounds", "Div Level", "Order",
             "Competition", "Reverse"};
+
         public override object[] ViewObjects
         {
             get
@@ -34,6 +35,34 @@ namespace JodyApp.ConsoleApp.Views
                 };
             }
         }
+
+        public override string[] EditHeaders => new string[] { "Name", "Home Type", "Home Team", "Home Div",
+            "Away Type", "Away Team", "Away Div", "Home&Away", "Rounds", "Div Level", "Order",
+            "Competition", "Reverse"};
+        public override object[] EditObjects
+        {
+            get
+            {
+                var m = (ScheduleRuleViewModel)Model;
+                return new object[]
+                {
+                    m.Name, m.HomeType,
+                    m.HomeTeam != null ? m.HomeTeam.Name : "None",
+                    m.HomeDivision != null ? m.HomeDivision.Name : "None",
+                    m.AwayType,
+                    m.AwayTeam != null ? m.AwayTeam.Name: "None",
+                    m.AwayDivision != null ? m.AwayDivision.Name: "None",
+                    m.PlayHomeAndAway,
+                    m.Rounds,
+                    m.DivisionLevel,
+                    m.Order,
+                    m.Competition != null ? m.Competition.Name : "None",
+                    m.Reverse
+                };
+
+            }
+        }
+
 
         public ScheduleRuleView(BaseViewModel model) : base(model) { }
     }
